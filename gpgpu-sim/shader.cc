4fa218b074f85b71a8aaab2d8e34c63e3a523d9b
ckiwun <ckiwun@gmail.com>
2015-11-29 13:54:02 -0800

    shadder.cc changed

--- gpgpu-sim/shader.cc
+++ gpgpu-sim/shader.cc
@@ -927,7 +927,7 @@ void scheduler_unit::cycle()
                     m_last_supervised_issued = supervised_iter;
                 }
             }
-            if(m_warpsplit_table.size()>0&&*iter == &m_warpsplit_table[0])
+            if(m_warpsplit_table.matched(*iter))
                 std::cout << "Run warpsplit" << std::endl;
     	    n_cyc_issue++;
             break;
